9f081dd67a8bd408ccbbc0112ba21bb1e8e92354
huepham1707 <huepham1707@gmail.com>
2022-09-06 15:28:40 +0700

    The 1st commit

---
+++ src/test/java/rmit/ios/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package rmit.ios.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
